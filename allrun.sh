5c4f6e4369c8512a299f83e6b02c604d51c0ea49
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2018-09-18 22:01:44 -0700

    cleanup

--- allrun.sh
+++ allrun.sh
@@ -2,8 +2,8 @@ mvn clean install
 
 docker rmi -f pgclient
 docker build -t pgclient .
-docker tag pgclient vdonthireddy/pgclient:2.0
-docker push vdonthireddy/pgclient:2.0
+docker tag pgclient vdonthireddy/pgclient:3.0
+docker push vdonthireddy/pgclient:3.0
 
 kubectl delete deploy pgclient-deployment & kubectl delete service pgclient-service
 kubectl apply -f aks.yml
